359dbd7ecd6b883debd1ae8f499bba070b713ef7
Pavan Valluri <pavanvalluri3730@gmail.com>
2026-09-15 04:10:58 -0400

    Add employee creation endpoint

---
+++ src/main/java/com/employee/api/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.employee.api.controller;
+
+import com.employee.api.model.Employee;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final List<Employee> employees = new ArrayList<>();
+    private long nextId = 1;
+
+    @PostMapping
+    public ResponseEntity<Employee> createEmployee(
+            @RequestBody Employee employee
+    ) {
+        employee.setId(nextId);
+        nextId++;
+
+        employees.add(employee);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(employee);
+    }
+}
